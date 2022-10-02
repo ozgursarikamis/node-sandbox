4ad6460b1a4b3df4c90c50fb181bd4b8dc5b293f
Ozgur SARIKAMIS <ozgur.sarikamis@smoothwall.com>
2022-10-02 15:33:03 +0300

    reading query string items with URL library

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const url = require('url');
 
 http.createServer(function (request, response) {
+	const queryStringItems = url.parse(request.url, true).query;
     const data = { message: "Hello Node World 234 567" };
     response.setHeader("Content-Type", "application/json");
-    response.end(JSON.stringify({ data: data, url: request.url }, null, 3));
+    response.end(JSON.stringify({ internal: data, parameters: queryStringItems }, null, 3));
 }).listen(8080);
