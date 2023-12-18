a6f306ea2bef8add18ab359c909087141b326b7c
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-12-18 03:01:55 +0000

    `Subject` Samples: Multicasting with `share` operator method

--- subjects/MulticastSubjectSample.ts
+++ subjects/MulticastSubjectSample.ts
@@ -1,8 +1,27 @@
-import { ConnectableObservable, Subject, from, multicast } from "rxjs";
+import { ConnectableObservable, Subject, from, multicast, share } from "rxjs";
 
 export class MulticastSubjectSample {
     public static Run() {
-        this.Multicasting();
+        // this.Multicasting();
+        this.MulticastingWithShareOperator();
+    }
+
+    static MulticastingWithShareOperator() {
+        // this method uses `share` operator to multicast
+        // share() is a modern version of `multicast(() => new Subject())`
+
+        const source$ = from([1, 2, 3, 4, 5]);
+        
+        const shared$ = source$.pipe(share());
+
+        shared$.subscribe({
+            next: (v) => console.log(`observerA: ${v}`)
+        });
+
+        shared$.subscribe({
+            next: (v) => console.log(`observerB: ${v}`)
+        });
+        
     }
 
     private static Multicasting() {
